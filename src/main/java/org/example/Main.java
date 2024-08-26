31657339eda08f49f0c57d99a677bb902c09af61
dudezi <a01036952858@gmail.com>
2024-08-26 10:52:30 +0900

    4단계완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import java.lang.module.FindException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("== 게시판 앱 ==");
+        int index = 1;
 
         while (true) {
             System.out.print("명령) ");
@@ -18,6 +20,9 @@ public class Main {
                 String subject = sc.nextLine();
                 System.out.print("내용 : ");
                 String content = sc.nextLine();
+
+                System.out.printf("%d번 게시글이 등록되었습니다.\n", index);
+                index++;
             }
         }
 
