0838d398a347de86652cf66df8edd6cb6b4f7825
dudezi <a01036952858@gmail.com>
2024-08-29 13:11:19 +0900

    ã이런

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class App {
+    Scanner sc;
+
+    App (Scanner sc) {
+        this.sc = sc;
+    }
+
+    public void run () {
+        int lastId = 1;
+        System.out.println("== 게시판 앱 ==");
+
+        while (true){
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+            if (command.trim().equals("종료")) {
+                break;
+            } else if (command.trim().equals("등록")) {
+                System.out.print("제목 : ");
+                String subject = sc.nextLine();
+                System.out.print("내용 : ");
+                String content = sc.nextLine();
+
+                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+                lastId++;
+            }
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,24 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int lastId = 1;
-        System.out.println("== 게시판 앱 ==");
-
-        while (true){
-            System.out.print("명령) ");
-            String command = sc.nextLine();
-            if (command.trim().equals("종료")) {
-                break;
-            } else if (command.trim().equals("등록")) {
-                System.out.print("제목 : ");
-                String subject = sc.nextLine();
-                System.out.print("내용 : ");
-                String content = sc.nextLine();
-
-                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
-                lastId++;
-            }
-        }
 
         sc.close();
     }
