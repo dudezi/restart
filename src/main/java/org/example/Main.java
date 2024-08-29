581b2f012dd5ab6a964954db93d18bfaf7ded05d
dudezi <a01036952858@gmail.com>
2024-08-29 13:48:27 +0900

    feat:start2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("== 게시판 앱 ==");
 
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+            if (command.trim().equals("종료")) {
+                break;
+            }
+        }
+        sc.close();
     }
 }
